47475733a42d3fd246c43e60ab3c3faabbf74fee
Randriamanantsoa Jordy <randriajordy8@gmail.com>
2024-12-12 11:39:38 +0100

    Design Hackaton et en fonction des dates

--- hackatWeb/src/Controller/HackatonController.php
+++ hackatWeb/src/Controller/HackatonController.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
 namespace App\Controller;
 
@@ -7,29 +7,44 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Hackaton;
+
 class HackatonController extends AbstractController 
 {
-    
     #[Route('/hackaton', name: 'app_hackaton')]
-    public function index( EntityManagerInterface $em): Response
+    public function index(Hackaton $ville,EntityManagerInterface $em): Response
     {   
         $repository = $em->getRepository(Hackaton::class);
-        $hackatons = $repository->findBy([], ['dateDebutHack' => 'DESC']);
-        $queryBuilder = $repository->createQueryBuilder('h')
-                ->where('h.dateDebutHack <= :today')  // Filtrer par date >= aujourd'hui
-             ->setParameter('today', new \DateTime())  // Passer la date d'aujourd'hui
-                ->orderBy('h.dateDebutHack', 'ASC');  // Trier par dateDebutHack de manière croissante
 
-// Exécuter la requête et obtenir les résultats
-        $hackatonsAncien = $queryBuilder->getQuery()->getResult();
-        return $this->render('hackaton/hackaton.html.twig', ['lesHackaton'=>$hackatons,'lesHackAncien'=>$hackatonsAncien
+        // Récupérer les hackathons à venir (date supérieure à aujourd'hui)
+        $queryBuilderFutur = $repository->createQueryBuilder('h')
+            ->where('h.dateDebutHack > :today') 
+            ->setParameter('today', new \DateTime()) 
+            ->orderBy('h.dateDebutHack', 'ASC'); // Hackathons futurs triés par ordre croissant
+        $hackatonsFuturs = $queryBuilderFutur->getQuery()->getResult();
+
+        // Récupérer les hackathons passés (date inférieure ou égale à aujourd'hui)
+        $queryBuilderAncien = $repository->createQueryBuilder('h')
+            ->where('h.dateDebutHack <= :today') 
+            ->setParameter('today', new \DateTime()) 
+            ->orderBy('h.dateDebutHack', 'DESC'); // Hackathons passés triés par ordre décroissant
+        $hackatonsAnciens = $queryBuilderAncien->getQuery()->getResult();
+
+        return $this->render('hackaton/hackaton.html.twig', [
+            'lesHackaton' => $hackatonsFuturs,
+            'lesHackAncien' => $hackatonsAnciens
         ]);
     }
-    #[Route('/Details/{id}',name :'app_details')]
-    public function details( int $id ,EntityManagerInterface $detail): Response {
-        $repository = $detail->getRepository(Hackaton::class);
+
+    #[Route('/Details/{id}', name: 'app_details')]
+    public function details(int $id, EntityManagerInterface $em): Response 
+    {
+        $repository = $em->getRepository(Hackaton::class);
         $detail = $repository->find($id);
-        return $this->render('hackaton/Details.html.twig', ['leDetails'=>$detail
-    ]);
+
+        return $this->render('hackaton/Details.html.twig', [
+            'leDetails' => $detail
+        ]);
     }
+    
+
 }
